e32bf8cecb47ff18ffd016f67cc55f59b34d9dd0
Tshireletso MENTOR <tmentor@c5r7s4.wethinkcode.co.za>
2019-10-24 16:19:33 +0200

    added

---
+++ account/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>index</title>
+    <link rel="stylesheet" href="../style/index.css">
+</head>
+<body>
+    <div class="middle_block">
+      <div class="sign_up">
+        <form>
+          <table>
+            <tr><td class="camagru">Camagru</td></tr>
+            <tr><td> <input type="email" placeholder="Email" value=""> </td></tr>
+            <tr><td> <input type="text" placeholder="Username" value=""> </td></tr>
+            <tr><td> <input type="password"  placeholder="Password" value=""> </td></tr>
+            <tr><td > <input type="submit"  value="sign up"> </td></tr>
+          </table>
+        </form>
+      </div>
+    </div>
+</body>
+</html>
---
+++ config/database.php
@@ -0,0 +1,7 @@
+<?PHP
+$DB_NAME = "camagru";
+$DB_SERVER = "mysql:host=localhost";
+$DB_SERVER_DB = "mysql:host=localhost;dbname=".$DB_NAME;
+$DB_USER = "root";
+$DB_PASSWORD = "tshireletso";
+?>
