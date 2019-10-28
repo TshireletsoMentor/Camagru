0e07623e0be3a8eaeae7406fc22d9ad70d01efa5
TshireletsoMentor <tmentor@student.wethinkcode.co.za>
2019-10-28 23:47:06 +0200

    added

--- account/index.php
+++ account/index.php
@@ -1,3 +1,27 @@
+<?PHP 
+include_once '../config/database.php';
+
+if(isset($_POST['email']) && isset($_POST['email']) && isset($_POST['username']) && isset($_POST['password'])){
+  $email = $_POST['email'];
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+
+  try{
+    $sql = "INSERT INTO `users` (username, email, password, verified)
+    VALUES (:username, :email, :password, :verified)";
+
+    $stmt = $conn->prepare($sql);
+    $stmt->execute(array(':username' => $username, ':email' => $email, ':password' => $password));
+  
+    if($stmt->rowCount() == 1){
+      $result = "Registration successful";
+    }
+  }
+  catch (PDOException $err){
+    $result = "Registration unsuccessful";;
+  }
+}
+?>
 
 <!DOCTYPE html>
 <html lang="en">
